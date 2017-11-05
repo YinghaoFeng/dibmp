6aaea03dc00db9bc0ab2fbbbeb09fd39140b8bf6
zhanglonggealj <694103334@qq.com>
2017-11-05 22:45:37 +0800

    test

--- dibmp-api/src/main/java/cn/mldn/dibmp/service/IMemberService.java
+++ dibmp-api/src/main/java/cn/mldn/dibmp/service/IMemberService.java
@@ -20,5 +20,5 @@ public interface IMemberService {
 	 * 1、key = allRoles、value = 该用户具备的所有角色；
 	 * 2、key = allActions、value = 该用户具备的所有权限
 	 */
-	public Map<String,Set<String>> getRoleAndActionByMember(String mid) ;
+	public Map<String,Set<String>> getRoleAndActionByMember(String mid) ;  
 }
